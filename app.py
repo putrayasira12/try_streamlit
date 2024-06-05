e32ec7cef6d84a95057dd13af7eb6162a2acb7aa
putrayasira12 <162549243+putrayasira12@users.noreply.github.com>
2024-06-05 17:04:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu 
 import home
 import eda
-import prediction
+# import prediction
 from PIL import Image
 
 st.set_page_config(
@@ -32,5 +32,5 @@ if selected == 'About':
     home.run()
 elif selected == 'EDA':
     eda.run()
-else:
-    prediction.run()
+# else:
+#     prediction.run()
